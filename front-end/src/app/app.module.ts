bcbbf8095147822fea9f8d0d1264b3193dcd41d6
Joao Alencar <joao.alencar19982@gmail.com>
2024-03-12 20:52:12 -0300

    adicionado interceptador de requisição para exibir alertas

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,17 +1,25 @@
-import { NgModule, importProvidersFrom } from '@angular/core';
+import { NgModule, Provider, importProvidersFrom } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductSaveComponent } from './components/product-save/product-save.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+
 import { InputTextModule } from 'primeng/inputtext';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AlertsHttpInterceptor } from './interceptors/alerts-http.interceptor';
+
+const alertsHttpInterceptor: Provider = {
+  provide: HTTP_INTERCEPTORS,
+  useClass: AlertsHttpInterceptor,
+  multi: true
+};
 
 @NgModule({
   declarations: [
@@ -26,11 +34,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     TableModule,
     ButtonModule,
     CardModule,
-    InputTextModule
+    InputTextModule,
   ],
   providers: [
     provideClientHydration(),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    alertsHttpInterceptor
   ],
   bootstrap: [AppComponent]
 })
---
+++ front-end/src/app/interceptors/alerts-http.interceptor.ts
@@ -0,0 +1,21 @@
+import { HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+
+@Injectable()
+export class AlertsHttpInterceptor implements HttpInterceptor {
+
+    constructor() { }
+
+    intercept(req: HttpRequest<any>, next: HttpHandler) {
+        return next.handle(req)
+            .pipe(
+                tap({
+                    next: (res) => { },
+                    error: (res) => {
+                        alert(res.message);
+                    }
+                }),
+            );
+    }
+}
