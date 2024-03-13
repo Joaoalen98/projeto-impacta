f027d50a7d25452d8711b3f6a120cd082c881b37
Joao Alencar <joao.alencar19982@gmail.com>
2024-03-12 21:18:46 -0300

    adicionado validação de DTOs

--- back-end/src/main/java/com/joaoalencar/projetoimpacta/service/dto/ProductDTO.java
+++ back-end/src/main/java/com/joaoalencar/projetoimpacta/service/dto/ProductDTO.java
@@ -1,5 +1,9 @@
 package com.joaoalencar.projetoimpacta.service.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,11 +13,18 @@ public class ProductDTO {
 
     private int id;
 
+    @NotNull(message = "Nome não pode ser nulo")
+    @NotBlank(message = "Nome não pode ficar em branco")
     private String name;
 
+    @NotNull(message = "Descrição não pode ser nulo")
+    @NotBlank(message = "Descrição não pode ficar em branco")
     private String description;
 
+    @NotNull(message = "Nome não pode ser nulo")
+    @DecimalMin(value = "0.01", message = "Preço não pode ser menor que 0,01")
     private float price;
 
+    @Min(value = 1, message = "Quantidade não pode ser menor que 1")
     private int stock;
 }
